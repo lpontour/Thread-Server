09219a70e1cbaca30f871ca1f6e8229749a91f4f
Lukas Paulmann <l.paulmann@ostfalia.de>
2018-06-27 19:10:21 +0200

    Kopletter Formatierer

---
+++ Thread_server/Server/FactoryFormate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Net.Sockets;
+
+namespace Server
+{
+	abstract class FactoryFormate : IFormatierer
+	{
+		#region methods
+		//Implementation der Schnittstelle, startet erzeugen eines konkreten Formatierers
+		public void Formatieren(NetworkStream socketStream, int formatnr)
+		{
+			Formatierer f = ErstelleFormatierer(formatnr, socketStream);
+		}
+
+
+		//Format für Formatierer waehlen
+		private Formatierer ErstelleFormatierer(int format, NetworkStream socketStream)
+		{
+			switch (format)
+			{
+				case 1:
+					return (FormatiereInStandert(socketStream));
+				case 2:
+					return (FormatiereInKeineLeerzeichen(socketStream));
+				case 3:
+					return (FormatiereInKeinZeilenumbruch(socketStream));
+				default:
+					return (FormatiereInStandert(socketStream));
+			}
+		}
+
+
+		//Konkretes erstellen des Formatierers mit dem gewünschten Format
+		private Formatierer CreatFormatierer(XmlWriterSettings format , NetworkStream socketStream)
+		{
+			return (new Formatierer(format, socketStream));
+		}
+
+
+		#region Formate
+
+		//Erzeugt die Formatierungseinstellungen für den Formatierer
+		private Formatierer FormatiereInStandert(NetworkStream socketStream)
+		{
+			XmlWriterSettings settings = new XmlWriterSettings { Indent = true, IndentChars = "  ", NewLineChars = "\r\n", NewLineHandling = NewLineHandling.Replace };
+
+			return (CreatFormatierer(settings, socketStream));
+		}
+
+
+		//Erzeugt die Formatierungseinstellungen für den Formatierer
+		private Formatierer FormatiereInKeineLeerzeichen(NetworkStream socketStream)
+		{
+			XmlWriterSettings settings = new XmlWriterSettings { Indent = true, IndentChars = "", NewLineChars = "\r\n", NewLineHandling = NewLineHandling.Replace };
+
+			return (CreatFormatierer(settings, socketStream));
+		}
+
+
+		//Erzeugt die Formatierungseinstellungen für den Formatierer
+		private Formatierer FormatiereInKeinZeilenumbruch(NetworkStream socketStream)
+		{
+			XmlWriterSettings settings = new XmlWriterSettings { Indent = true, IndentChars = "  ", NewLineChars = "\r", NewLineHandling = NewLineHandling.Replace };
+
+			return (CreatFormatierer(settings, socketStream));
+		}
+		#endregion
+
+		#endregion
+	}
+}
+
