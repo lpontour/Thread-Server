553b9cf3e512c066dd404effd9974f9ee2530dff
Lukas Paulmann <l.paulmann@ostfalia.de>
2018-06-27 19:33:11 +0200

    Kompletter Formatierer v2

---
+++ Thread_server/Server/IFormatierer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Net.Sockets;
+
+namespace Server
+{
+	interface IFormatierer
+	{
+		#region methods
+		//Bereitstellen der Schnittstelle zum erzeugen und benutzen einers XML-Formatierers 
+		void Formatieren(NetworkStream socketStream, int formatnr);
+		#endregion
+	}
+}
