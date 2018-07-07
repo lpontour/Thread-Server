ca1b6fcf710665a404b5378a41e9aea9d1512bad
Dana <dana.lachmann@web.de>
2018-07-07 17:47:44 +0200

    Konsolenkommunikation für Eingansparameter; Umformen der Semaphore

--- Thread_server/Server/SocketVerbindung.cs
+++ Thread_server/Server/SocketVerbindung.cs
@@ -12,24 +12,63 @@ namespace Server
     internal class SocketVerbindung
     {
         #region fields
-        // Semaphore erzeugen
-        int maxVerbindungen = 2;
-        private static Semaphore semaphore = new Semaphore(2, 2);
-        // Konfiguration Server
-        int port = 3;
-        IPAddress ipAdresse = IPAddress.Any;
-        // Verbindung
-        string nachricht;
-        byte[] buffer = new Byte[256];
-        string data = null;
-        #endregion
+        // Anzahl der maximalen Verbindungen
+        internal int maxVerbindungen;
 
+        // Konfigiuration des Servers
+        internal int port;
+        internal IPAddress ipAdresse;
+
+        // Socket
+        internal Socket socket;
+
+        #endregion
 
         #region ctor
         internal SocketVerbindung()
         {
-            Thread wartenThread = new Thread(new ThreadStart(this.WartenAufVerbindung));
-            wartenThread.Start();
+            // Ermitteln der maximalen Verbindungen durch Konsoleneingabe
+            bool erlaubt = false;
+            while(erlaubt == false)
+            {
+                Console.WriteLine("Wie viele maximale Verbindungen sind erlaubt? \nMit Enter wird der Standardwert von 2 Verbindungen übernommen.");
+                string eingabe = Console.ReadLine();
+                if (eingabe == "")
+                {
+                    maxVerbindungen = 2;
+                    erlaubt = true;
+                }
+                else
+                {
+                    erlaubt = int.TryParse(eingabe, out maxVerbindungen);
+                }
+            }
+            
+            // Ermitteln der IP-Adresse durch Konsoleneingabe
+            erlaubt = false;
+            while (erlaubt == false)
+            {
+                Console.WriteLine("Welche IP-Adresse wird genutzt?");
+                string eingabe = Console.ReadLine();
+                erlaubt = IPAddress.TryParse(eingabe, out ipAdresse);
+            }
+
+            // Ermitteln des Ports durch Konsoleneingabe
+            erlaubt = false;
+            while (erlaubt == false)
+            {
+                Console.WriteLine("Welcher Port soll genutzt werden?");
+                string eingabe = Console.ReadLine();
+                erlaubt = int.TryParse(eingabe, out port);
+            }
+            
+            Console.ReadKey();
+
+            // Dauerhaftes warten auf Verbindungsanfragen des Clients
+            while(true)
+            { 
+                WartenAufVerbindung();
+            }
         }
         #endregion
 
@@ -37,29 +76,47 @@ namespace Server
         #region methods
         internal void WartenAufVerbindung()
         {
-            TcpListener listenerServer = new TcpListener(ipAdresse, port);
+            // Initialisieren und starten des TCPListeners
+            TcpListener listenerServer;
+            listenerServer = new TcpListener(ipAdresse, port);
+            listenerServer.Start();
 
-            try
+            // Variable zum Blockieren des kritischen Codes
+            bool semaLock = false;
+
+            // Erzeugen der Semaphore...
+            Semaphore semaphore = new Semaphore(maxVerbindungen, maxVerbindungen);
+
+            // ...wenn eine Verbindungsanfrage besteht...
+            if (listenerServer.Pending())
             {
-                listenerServer.Start();
-                Socket socket = listenerServer.AcceptSocket();
+                // ...und die Semaphore noch nicht blockiert wird,...
+                if (!semaLock)
+                {
+                    try
+                    {
+                        // ...wird ein neuer Thread erstellt 
+                        new Thread (() =>
+                        {
+                            Thread.CurrentThread.IsBackground = true;
+                            // Blockieren ist abhängig vom Status der Semaphore + WaitOne dekrementiert die Semaphore
+                            semaLock = semaphore.WaitOne();
+                            // Annehmen der Socketverbindung
+                            socket = listenerServer.AcceptSocket();
 
+                            // Übergeben des Sockets
+
+                            // Release inkrementiert die Semaphore
+                            semaphore.Release();
+                        });
+                    }
+                    catch (Exception exception)
+                    {
+                        throw new Exception("Fehler bei Verbindung", exception);
+                    }
+                }
             }
-            catch (Exception exception)
-            {
-                throw new Exception("Fehler bei Verbindungserkennung", exception);
-            }
 
-
-        }
-
-        internal void VerbindungSemaphore()
-        {
-            semaphore.WaitOne();
-
-
-
-            semaphore.Release();
         }
         #endregion
 
