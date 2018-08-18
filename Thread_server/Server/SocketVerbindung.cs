f8a29462b56998a605be5aa779ad5a90f6614330
Lukas Paulmann <l.paulmann@ostfalia.de>
2018-08-18 15:17:47 +0200

    nvbnc

--- Thread_server/Server/SocketVerbindung.cs
+++ Thread_server/Server/SocketVerbindung.cs
@@ -107,7 +107,7 @@ namespace Server
 									string str;
 									//using (stream)
 									{
-										byte[] data = new byte[2048];
+										byte[] data = new byte[1024];
 										using (MemoryStream ms = new MemoryStream())
 										{
 
